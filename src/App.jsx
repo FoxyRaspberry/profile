3921f3ad97b2953a28638c14223162823e2467aa
Лисичкина Марина <m.lisichkina@lucky-team.pro>
2025-10-26 20:56:40 +0300

    build: add configuration files for a React site

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Header } from './Header/Header-section-component.jsx'
+
+export default function App() {
+  return (
+    <>
+      <div className='wrapper'>
+        <Header></Header>
+      </div>
+    </>
+  )
+}
