04c0882f29f5ab41db994d37e3882ae268337c23
Лисичкина Марина <m.lisichkina@lucky-team.pro>
2025-10-27 17:35:11 +0300

    feat: change the markup, add a language switcher for `Header-section-component`

--- src/Header/Header-section-component.jsx
+++ src/Header/Header-section-component.jsx
@@ -4,8 +4,17 @@ import './Header-section-component.css'
 export function Header () {
   return (
     <>
-      <div className='header-section-logo-container'>
-        <img src={reactLogo} className='header-section-logo' />
+      <div className='header-section'>
+        <div className='wrapper'>
+          <div className='header-section__item-container'>
+            <div className='header-section-logo-container'>
+              <h1 className='header-section-logo'>Lisichkina</h1>
+            </div>
+            <div className='header-section__language-container'>
+              <a className='header-section__language'>EN|RU</a>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
