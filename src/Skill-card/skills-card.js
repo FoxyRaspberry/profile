7e6123729237a5f52efcd64eee7e5d4a6b196d31
Лисичкина Марина <m.lisichkina@lucky-team.pro>
2025-10-28 20:28:26 +0300

    feat: add data with images for `Skill-card-component`

---
+++ src/Skill-card/skills-card.js
@@ -0,0 +1,53 @@
+import htmlImage from '../assets/html.svg';
+import cssImage from '../assets/css.svg';
+import jsImage from '../assets/js.svg';
+import reactImage from '../assets/react.svg';
+import figmaImage from '../assets/figma.svg';
+import wordpressImage from '../assets/wordpress.svg';
+import vscodeImage from '../assets/vscode.svg';
+import viteImage from '../assets/vite.svg';
+import githubImage from '../assets/github.svg';
+import angularImage from '../assets/angular.svg';
+
+export const skillsCard = [
+  {
+    id: 1,
+    image: htmlImage,
+  },
+  {
+    id: 2,
+    image: cssImage,
+  },
+  {
+    id: 3,
+    image: jsImage,
+  },
+  {
+    id: 4,
+    image: reactImage,
+  },
+  {
+    id: 5,
+    image: figmaImage,
+  },
+  {
+    id: 6,
+    image: wordpressImage,
+  },
+  {
+    id: 7,
+    image: vscodeImage,
+  },
+  {
+    id: 8,
+    image: viteImage,
+  },
+  {
+    id: 9,
+    image: githubImage,
+  },
+  {
+    id: 10,
+    image: angularImage,
+  },
+]
