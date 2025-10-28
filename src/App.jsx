538fa9158d2ab9ab63df369e2755f8a1ec1c72fc
Лисичкина Марина <m.lisichkina@lucky-team.pro>
2025-10-28 12:03:53 +0300

    feat: create and import `Skills-section-component`

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Header } from './Header/Header-section-component.jsx'
 import { Hero } from './Hero/Hero-section-component.jsx'
+import { Skills } from './Skills/Skills-section-component.jsx'
 
 export default function App() {
   return (
     <>
       <Header></Header>
       <Hero></Hero>
+      <Skills></Skills>
     </>
   )
 }
---
+++ src/Skills/Skills-section-component.jsx
@@ -0,0 +1,16 @@
+import './Skills-section-component.css';
+
+export function Skills() {
+  return (
+    <>
+      <div className='skills-section'>
+        <div className='wrapper'>
+          <div className='skills-section__item-container'>
+            <h1 className='skills-section__title'>Skills</h1>
+            <p className='skills-section__subtitle'>The main technologies and tools that I own.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
